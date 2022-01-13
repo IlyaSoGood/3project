e81782a8ddcfe7de94695aa25d76537d6fb08a79
IlyaSoGood <87233953+IlyaSoGood@users.noreply.github.com>
2022-01-13 23:55:40 +0300

    Added php.mailer on OperServer and wrote JS code for him

--- src/js/script.js
+++ src/js/script.js
@@ -51,16 +51,14 @@ document.querySelector('.next').addEventListener('click', function () {
   });
 
 
-$(document).ready(function() {
-    $(function() {
+$(document).ready(function(){
         
-        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-        });
-        
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+        
 
 
     function toggleSlide(item) {
@@ -92,10 +90,13 @@ $(document).ready(function() {
         })
     });
     
-    function valideForms(form) {
+    function validateForms(form) {
         $(form).validate({
             rules: {
-                name: "required",
+                name: {
+                    required: true,
+                    minlength: 2
+                },
                 phone: "required",
                 email: {
                     required: true,
@@ -112,9 +113,29 @@ $(document).ready(function() {
               }
         })
     };
-    valideForms('#consultation-form');
-    valideForms('#consultation form');
-    valideForms('#order form');
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        };
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
